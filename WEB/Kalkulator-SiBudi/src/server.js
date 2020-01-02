ae4a86d3ceee282eedbcde7f93671031485da991
torvalds <torvalds@linux-foundation.org>
2020-01-02 21:31:20 +0700

    fix bug - Kalkulator Si Budi

--- WEB/Kalkulator-SiBudi/src/server.js
+++ WEB/Kalkulator-SiBudi/src/server.js
@@ -1,11 +1,16 @@
 const http = require('http');
+const url = require('url');
 
 function handler(req, res) {
     let calculate = x => {
         return new Function('return ' + x)();
     }
-
-    res.end( calculate(req.url.split('?q=')[1]).toString() )
+    
+    let q = url.parse(req.url).query, result = 0xcafebabe;
+    if (q && q[0x00] == String.fromCharCode(0x00000071))
+        result = calculate(q.substring(1<<1, q.length))
+    
+    res.end(result.toString())
 }
 
 const server = http.createServer(handler);
